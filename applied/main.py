9969bb748db18ae3db2488b4663d81e0df860a57
lspgl <leo.spiegel@bluewin.ch>
2018-01-16 14:14:14 +0100

    Multiple measurements

--- applied/main.py
+++ applied/main.py
@@ -7,12 +7,29 @@ sys.path.append(__location__)
 from sequence import Sequence
 from pair import Pair
 
+import traceback
+
 
 def main():
-    s = Sequence(offsite=False)
+    s = Sequence(offsite=True)
     s.prime()
-    pair = s.measure(n=16)
-    pair.store()
+    continuous = False
+    if continuous:
+        while True:
+            try:
+                pair = s.measure(n=16)
+                pair.store()
+                del pair
+            except KeyboardInterrupt:
+                s.disable()
+                sys.exit()
+            except:
+                del pair
+                s.disable()
+                traceback.print_exc()
+    else:
+        pair = s.measure(n=16)
+        pair.store()
     # for i in range(16):
     #    pair = Pair('CSAT_' + str(i + 1) + '.h5', fromFile=True, corrections=True)
     #    pair.plot()
